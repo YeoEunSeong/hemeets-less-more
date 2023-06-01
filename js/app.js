43c8f3ee19044dc2a4fb3aa2fba15354088f95f2
Eunseong, Yeo <e7838752@gmail.com>
2023-06-01 21:22:47 +0900

    add title for desktop

--- js/app.js
+++ js/app.js
@@ -174,6 +174,7 @@ const shuffle = arr => {
 
 const render = () => {
   root.innerHTML = `
+  <h1 class="title">히미츠 더 많이 더 적게</h1>
   <div class="container">
     <div class="left">
       <h2 class="left-title">${left.name}</h2>
